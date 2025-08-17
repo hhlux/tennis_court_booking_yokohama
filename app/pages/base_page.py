808a316f2d01cf15e15e6966531e390600e8b75a
ciying <heciying@takasyou.cloud>
2025-08-17 12:51:16 +0900

    dev

--- app/pages/base_page.py
+++ app/pages/base_page.py
@@ -1,4 +1,5 @@
 import platform
+import random
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -22,20 +23,27 @@ class BasePage:
             prefs = {
                 'download.prompt_for_download': False
             }
+            user_agent_list = [
+                # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+                # 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+                # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+                # 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+            ]
             options.add_experimental_option('prefs', prefs)
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-gpu')
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument('--disable-application-cache')
             options.add_argument('window-size=1920,1080')
+            options.add_argument('--user-agent=' + random.choice(user_agent_list))
             if platform.system() != 'Darwin':  # For Cloud Run
                 options.binary_location = '/usr/bin/google-chrome'
                 options.add_argument('--headless=new')
             # options.add_experimental_option("prefs", {"download.default_directory": str(const.DOWNLOAD_FOLDER.absolute())})
-            # BasePage.driver = webdriver.Remote(command_executor='http://selenium-hub:4444/wd/hub', options=options)
             BasePage.driver = webdriver.Chrome(options=options)
             BasePage.driver.maximize_window()
-            # BasePage.driver.implicitly_wait(10)
+            BasePage.driver.implicitly_wait(1)
             BasePage.wait = WebDriverWait(BasePage.driver, 10)
             BasePage.wait_long = WebDriverWait(BasePage.driver, 60)
             BasePage.wait_super_long = WebDriverWait(BasePage.driver, 600)
